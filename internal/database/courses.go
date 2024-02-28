a72815b0bb89ffa0e2f5880cba4c592c5af4436d
Matheus da Silva de Paula <matheus.silva@startyourapp.com.br>
2024-02-28 18:00:22 -0300

    feat: create model and methods

---
+++ internal/database/courses.go
@@ -0,0 +1,60 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+)
+
+type Courses struct {
+	db          *sql.DB
+	ID          string
+	Name        string
+	Description string
+	CategoryId  string
+}
+
+func (c *Courses) Create(name, description, categoryId string) (*Courses, error) {
+	id := uuid.New().String()
+
+	_, err := c.db.Exec("INSERT INTO courses (id, name, description, category_id) VALUES ($1, $2, $3, $4)", id, name, description, categoryId)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Courses{
+		ID:          id,
+		Name:        name,
+		Description: description,
+		CategoryId:  categoryId,
+	}, nil
+}
+
+
+func (c *Courses) FindAll() ([]Courses, error) {
+	rows, err := c.db.Query("SELECT id, name, description, category_id FROM courses")
+
+	if err != nil {
+		return nil, err
+
+	}
+
+	defer rows.Close()
+
+	courses := []Courses{}
+
+	for rows.Next() {
+		var id, name, description, categoryId string
+		if err:= rows.Scan(&id, &name, &description, &categoryId); err != nil {
+			return nil, err
+		}
+		courses = append(courses, Courses{
+			ID:          id,
+			Name:        name,
+			Description: description,
+			CategoryId:  categoryId,
+		})
+	}
+	return courses, nil
+}
